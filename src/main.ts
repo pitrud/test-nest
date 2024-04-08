6fc58042b59baa9d0049208f78ea82f04fb676df
Piotr Rudnicki <prudnicki@plog.pl>
2024-04-08 14:09:41 +0200

    test commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Nest API')
@@ -11,6 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('/api', app, document);
 
   await app.listen(3000);
